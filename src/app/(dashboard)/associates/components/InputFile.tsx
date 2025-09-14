045cc07e391ee17aac8011357f4bde09e367ba4f
enzoleao <enzopinheiro6@gmail.com>
2025-09-13 23:44:02 -0300

    feat: adicionando trigger correto no input de fotos

--- src/app/(dashboard)/associates/components/InputFile.tsx
+++ src/app/(dashboard)/associates/components/InputFile.tsx
@@ -17,14 +17,14 @@ export function InputFile() {
   };
 
   return (
-    <div className="relative flex items-center justify-center gap-3 rounded-full h-[96px] w-[96px] bg-gray-100 border-2 border-dashed hover:bg-gray-200 hover:border-blue-500 transition-all">
-      <div className="relative w-full h-full cursor-pointer">
+    <div className="relative flex items-center justify-center gap-3 rounded-full h-[96px] w-[96px] bg-gray-100 border-2 border-dashed hover:bg-gray-200 hover:border-blue-500 transition-all cursor-pointer">
+      <div className="relative w-full h-full">
         <Input
           id="picture"
           type="file"
-          className="opacity-0 w-full h-full cursor-pointer z-10"
+          className="opacity-0 absolute inset-0 w-full h-full cursor-pointer z-10"
           accept="image/*"
-          onChange={handleFileChange} 
+          onChange={handleFileChange}
         />
         
         {imagePreview ? (
@@ -34,10 +34,10 @@ export function InputFile() {
             className="absolute inset-0 w-full h-full object-cover rounded-full"
           />
         ) : (
-          <IconCamera className="absolute inset-0 m-auto text-gray-500 cursor-pointer" size={24} />
+          <IconCamera className="absolute inset-0 m-auto text-gray-500" size={24} />
         )}
       </div>
-      
+
       <div className="absolute bottom-0 right-0 bg-blue-500 p-1 rounded-full cursor-pointer">
         <IconPencilMinus className=" text-white rounded-full cursor-pointer" size={20} />
       </div>
